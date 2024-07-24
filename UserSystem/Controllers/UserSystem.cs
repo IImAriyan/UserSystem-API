061a7d9e4bdd062ad19b14ec3c1754201bac7425
MRHidden100 <adibariyanesmaeilzadeh@gmail.com>
2024-07-24 21:50:41 +0330

    Add User Function

--- UserSystem/Controllers/UserSystem.cs
+++ UserSystem/Controllers/UserSystem.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using UserSystem.Entitys;
 
 namespace UserSystem.Controllers
@@ -9,10 +10,22 @@ namespace UserSystem.Controllers
     [ApiController]
     public class UserSystem(DbContextApp dbContext) : ControllerBase
     {
+        // Show Users Async Function
         [HttpGet("Users/list")]
         public  ActionResult<IEnumerable<UsersEntity>> ShowUsers()
         {
             return  dbContext.Users;
+        }
+        
+        // Add User Function
+        [HttpPost("Users/Add")]
+        public async Task<ActionResult<UsersEntity>> AddUser(UsersEntity dto)
+        {
+            EntityEntry<UsersEntity> User = await dbContext.AddAsync(dto);
+            
+            dbContext.SaveChangesAsync();
+            
+            return Ok(User);
         }
     }
 }
