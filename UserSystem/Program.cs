0ee92c656c7afc5d270adcaac073c95a8b3ae943
MRHidden100 <adibariyanesmaeilzadeh@gmail.com>
2024-07-24 21:14:19 +0330

    Create User Controller And Install Nuget Packages And Run Swagger

--- UserSystem/Program.cs
+++ UserSystem/Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,6 +17,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
 
