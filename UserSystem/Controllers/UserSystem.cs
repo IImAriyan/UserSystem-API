26f0e7ccb8524085033eee0a7455239b37c4ff23
MRHidden100 <adibariyanesmaeilzadeh@gmail.com>
2024-07-24 21:42:09 +0330

    Create Show Users Function

--- UserSystem/Controllers/UserSystem.cs
+++ UserSystem/Controllers/UserSystem.cs
@@ -1,11 +1,18 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UserSystem.Entitys;
 
 namespace UserSystem.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api")]
     [ApiController]
-    public class UserSystem : ControllerBase
+    public class UserSystem(DbContextApp dbContext) : ControllerBase
     {
+        [HttpGet("Users/list")]
+        public  ActionResult<IEnumerable<UsersEntity>> ShowUsers()
+        {
+            return  dbContext.Users;
+        }
     }
 }
